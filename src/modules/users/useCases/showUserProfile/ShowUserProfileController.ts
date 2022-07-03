c7ec3ed677b7f1ff47d98be5afd9c8b8a4cf6d6b
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-07-03 21:25:09 +0000

    feat: implemet user search route

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,19 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    try {
+      const targetUser = this.showUserProfileUseCase.execute({ user_id });
+
+      return response.json(targetUser);
+    } catch (error) {
+      const typedError = error as Error;
+
+      return response.status(404).json({
+        error: typedError.message,
+      });
+    }
   }
 }
 
--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -9,7 +9,13 @@ class ShowUserProfileUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const targetUser = this.usersRepository.findById(user_id);
+
+    if (!targetUser) {
+      throw new Error("User not exists");
+    }
+
+    return targetUser;
   }
 }
 
